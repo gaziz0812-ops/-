0d96089f635c6395a5374b9a65a11623cae0e984
Илья <gaziz0812@gmail.com>
2026-05-26 22:13:56 +0300

    Добавил основные приложения Django

--- japanese_sword/japanese_sword/settings.py
+++ japanese_sword/japanese_sword/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
+import manufacturers
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 ENV_FILE = BASE_DIR.parent / ".env"
@@ -69,6 +71,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'manufacturers',
+    'products',
+    'stock',
+    'supplies',
+    'sales',
 ]
 
 MIDDLEWARE = [
