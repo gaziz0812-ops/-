3e7123e5f9ea23eabf27a0e8685ce92cdb0a7398
Илья <gaziz0812@gmail.com>
2026-05-27 13:20:44 +0300

    Добавил модели производителей и товаров

--- japanese_sword/manufacturers/models.py
+++ japanese_sword/manufacturers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True)  # имя производител = 255 длина строки, уникальная строка
+    contacts = models.TextField(blank=True)  # контакты = поле может быть пустым
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
--- japanese_sword/products/models.py
+++ japanese_sword/products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    sku = models.CharField(max_length=64, unique=True)  # номер артикула
+    name = models.CharField(max_length=255)  # название артикула
+    manufacturer = models.ForeignKey(  # переменная для связи продуктов с родительским Manufacturer
+        'manufacturers.Manufacturer',  # ссылка на класс мануфактуры
+        on_delete=models.PROTECT,  # запрещает удалять производителя если от него есть продукты
+        related_name='products',  # обратная связь
+    )
+    description = models.TextField(blank=True)  # характеристики продукта
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2)  # цена (максимум 10 цифр и 2 точки)
+    is_active = models.BooleanField(default=True)  # Продукт по дефолту активен, учтен в остатках и в каталоге
+
+    def __str__(self):
+        return f"{self.sku} - {self.name}"
+
+
